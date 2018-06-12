2201c556ed806350487748e7447cba576c46d983
9 <1756211858@qq.com>
2018-06-12 15:48:24 +0800

    更新代码
    第一次更新代码  但大几率是最后一次

--- app/src/main/java/com/nine/Wechat/AutoService.java
+++ app/src/main/java/com/nine/Wechat/AutoService.java
@@ -3,49 +3,53 @@ package com.nine.Wechat;
 import android.accessibilityservice.AccessibilityService;
 import android.content.ClipData;
 import android.content.ClipboardManager;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Handler;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
-import android.widget.Toast;
 
 import java.util.List;
-import java.util.concurrent.Delayed;
 
 import static android.content.ContentValues.TAG;
+
 /*
             手动点击item
  */
 public class AutoService extends AccessibilityService {
-    private static boolean prepos=true; //当前的页面
-    private int page = 1;//默认页数
-    private static int index = 0;
+    private static boolean prepos = true; //当前的页面
+    private int index = 0;
     private int eventType;
     private String data;//接受传递来的请求内容
-    private static String className=null;
+    private static String className = null;
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         eventType = event.getEventType();
         className = event.getClassName().toString();
 
-        switch (className) {
-            case "com.tencent.mm.plugin.nearby.ui.NearbyFriendsUI":
-                //addFriends();
-                Log.e("mlog","addfriends");
-                break;
-            case "com.tencent.mm.plugin.profile.ui.ContactInfoUI":
-                greet();
-                break;
-        }
+                switch (className) {
+                    case "com.tencent.mm.plugin.nearby.ui.NearbyFriendsUI":
+                        addFriends();
+                        Log.e("mlog", "addfriends");
+                        break;
+                    case "com.tencent.mm.plugin.profile.ui.ContactInfoUI":
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                greet();
+                            }
+                        }, 1000);
+                        break;
+                }
 
-}
+    }
+
     @Override
     public void onInterrupt() {
 
     }
+
     /*
             当服务绑定成功
     */
@@ -53,48 +57,42 @@ public class AutoService extends AccessibilityService {
     protected void onServiceConnected() {
         super.onServiceConnected();
         SharedPreferences settings = getSharedPreferences("Wechat_content", 0);
-        data=settings.getString("content","嘤嘤嘤 我就是9岁嘛");
+        data = settings.getString("content", "嘤嘤嘤 我就是9岁嘛");
     }
 
     /*
         点击添加附近的人item
          */
     private void addFriends() {
-     new Handler().postDelayed(new Runnable() {
-         @Override
-         public void run() {
-             index += 1;
-             AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-             List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/cjc");
-             if (list.size()!=0) {
-                 List<AccessibilityNodeInfo> username=nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/cjp");
-                 if(username.size()!=0) {
-                     if (username.get(index).getParent() != null) {
-                         username.get(index).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                         username.get(index).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+                List<AccessibilityNodeInfo> username = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/b0p");
+                if (index == username.size() - 1) {
+                    index = 0;
+                    //滑动
+                    AccessibilityNodeInfo nodeInfo_ = getRootInActiveWindow();
+                    List<AccessibilityNodeInfo> list_ = nodeInfo_.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/cjc");
+                    list_.get(0).performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
+                    Log.d("mlog", "列表人数: " + list_.size());
+                } else {
+                    username.get(index).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    index += 1;
 
-                     }
-                 }
+                }
+            }
+        },1000);
 
-             } else if (index == list.size()) {
-                         //滑动
-                         AccessibilityNodeInfo node_lsv = nodeInfo.getChild(0).getChild(index);
-                         node_lsv.performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
-
-             }
-         }
-     },300);
     }
+
 
     /*
     详细资料
      */
     private void greet() {
-new Handler().postDelayed(new Runnable() {
-    @Override
-    public void run() {
         List<AccessibilityNodeInfo> infos = getRootInActiveWindow().findAccessibilityNodeInfosByText("打招呼");
-        if(infos.size()>0&&prepos) {
+        if (infos.size() > 0 && prepos) {
             for (AccessibilityNodeInfo item : infos) {
                 item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                 new Handler().postDelayed(new Runnable() {
@@ -102,26 +100,23 @@ new Handler().postDelayed(new Runnable() {
                     public void run() {
                         inputHello(data);
                     }
-                },200);
+                }, 200);
             }
-        }else{
-            index++;
-            prepos=true;
+        } else {
+            prepos = true;
             performGlobalAction(GLOBAL_ACTION_BACK);
         }
     }
-},400);
 
-    }
     /*
      * 发送请求
      */
     private void sendIt() {
 
-        List<AccessibilityNodeInfo> infos =  getRootInActiveWindow().findAccessibilityNodeInfosByText("发送");
+        List<AccessibilityNodeInfo> infos = getRootInActiveWindow().findAccessibilityNodeInfosByText("发送");
         for (AccessibilityNodeInfo item : infos) {
-                item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            prepos=false;
+            item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            prepos = false;
         }
 
     }
@@ -151,7 +146,7 @@ new Handler().postDelayed(new Runnable() {
             public void run() {
                 sendIt();
             }
-        },400);
+        }, 1000);
     }
 
 }
